25d18bd4cb68bf8fa6afa092b83c6bb75176f33c
Vernon Wells <v.wells@f5.com>
2025-04-01 00:43:56 -0700

    StreamExtractors tests added

--- extractor_test.go
+++ extractor_test.go
@@ -1,30 +1,229 @@
 package mps_test
 
 import (
-	"testing"
+	"fmt"
 
 	"github.com/blorticus-go/mocks"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
 	"github.com/blorticus-go/mps"
 )
 
-func TestStreamExtractor(t *testing.T) {
-	lengthExtractor := func(accumulatedData []byte) (lengthOfNextMessage uint64, err error) {
-		if len(accumulatedData) < 4 {
-			return 0, nil
-		}
-		return uint64(accumulatedData[4]), nil
-	}
+var _ = Describe("Extractors", func() {
+	Describe("StreamExtractors", func() {
+		var lengthExtractor func([]byte) (uint64, error)
+		var reader *mocks.Reader
+		var streamExtractor *mps.StreamExtractor
+		var eventChannel chan *mps.ExtractionEvent
 
-	r := mocks.NewReader().
-		AddEOF()
+		BeforeEach(func() {
+			lengthExtractor = func(accumulatedData []byte) (lengthOfNextMessage uint64, err error) {
+				if len(accumulatedData) < 5 {
+					return 0, nil
+				}
+				return uint64(accumulatedData[4]), nil
+			}
 
-	c := make(chan *mps.ExtractionEvent)
-	e := mps.NewStreamExtractor(lengthExtractor)
+			reader = mocks.NewReader()
+			streamExtractor = mps.NewStreamExtractor(lengthExtractor)
+			eventChannel = make(chan *mps.ExtractionEvent)
+		})
 
-	go e.StartExtracting(r, c)
+		When("the reader returns EOF immediately", func() {
+			BeforeEach(func() {
+				reader.AddEOF()
+				go streamExtractor.StartExtracting(reader, eventChannel)
+			})
 
-	event := <-c
-	if event.Type != mps.StreamClosed {
-		t.Errorf("Expected StreamClosed event, got %v", event.Type)
-	}
-}
+			It("should emit a StreamClosed event", func() {
+				reader.AddEOF()
+				go streamExtractor.StartExtracting(reader, eventChannel)
+
+				event := <-eventChannel
+				Expect(event.Type).To(Equal(mps.StreamClosed))
+			})
+		})
+
+		When("the reader returns an inital error", func() {
+			BeforeEach(func() {
+				reader.AddError(fmt.Errorf("Test error"))
+				go streamExtractor.StartExtracting(reader, eventChannel)
+			})
+
+			It("should emit an ExtractionError event", func() {
+				event := <-eventChannel
+				Expect(event.Type).To(Equal(mps.ExtractionError))
+			})
+		})
+
+		When("the reader returns a single valid message of length 10 in one read", func() {
+			BeforeEach(func() {
+				reader.AddGoodRead([]byte{0, 1, 2, 3, 10, 4, 5, 6, 7, 8}).AddEOF()
+				go streamExtractor.StartExtracting(reader, eventChannel)
+			})
+
+			It("should emit the correct events", func() {
+				By("waiting for the first event")
+				event := <-eventChannel
+				Expect(event.Type).To(Equal(mps.MessageReceived))
+				Expect(event.Message).To(Equal([]byte{0, 1, 2, 3, 10, 4, 5, 6, 7, 8}))
+
+				By("waiting for the second event")
+				event = <-eventChannel
+				Expect(event.Type).To(Equal(mps.StreamClosed))
+			})
+		})
+
+		When("the reader returns a single valid message of length 20 in two reads", func() {
+			When("the first read contains the length", func() {
+				BeforeEach(func() {
+					reader.
+						AddGoodRead([]byte{0, 1, 2, 3, 10, 4}).
+						AddGoodRead([]byte{5, 6, 7, 8}).
+						AddEOF()
+					go streamExtractor.StartExtracting(reader, eventChannel)
+				})
+
+				It("should emit the correct events", func() {
+					By("waiting for the first event")
+					event := <-eventChannel
+					Expect(event.Type).To(Equal(mps.MessageReceived))
+					Expect(event.Message).To(Equal([]byte{0, 1, 2, 3, 10, 4, 5, 6, 7, 8}))
+
+					By("waiting for the second event")
+					event = <-eventChannel
+					Expect(event.Type).To(Equal(mps.StreamClosed))
+				})
+			})
+
+			When("the second read contains the length", func() {
+				BeforeEach(func() {
+					reader.
+						AddGoodRead([]byte{0, 1, 2}).
+						AddGoodRead([]byte{3, 10, 4, 5, 6, 7, 8}).
+						AddEOF()
+					go streamExtractor.StartExtracting(reader, eventChannel)
+				})
+
+				It("should emit the correct events", func() {
+					By("waiting for the first event")
+					event := <-eventChannel
+					Expect(event.Type).To(Equal(mps.MessageReceived))
+					Expect(event.Message).To(Equal([]byte{0, 1, 2, 3, 10, 4, 5, 6, 7, 8}))
+
+					By("waiting for the second event")
+					event = <-eventChannel
+					Expect(event.Type).To(Equal(mps.StreamClosed))
+				})
+			})
+		})
+
+		When("the reader returns two valid messages", func() {
+			Context("in a single read", func() {
+				BeforeEach(func() {
+					reader.
+						AddGoodRead([]byte{0, 1, 2, 3, 5, 10, 20, 30, 50, 7, 60, 70}).
+						AddEOF()
+					go streamExtractor.StartExtracting(reader, eventChannel)
+				})
+
+				It("should emit the correct events", func() {
+					By("waiting for the first event")
+					event := <-eventChannel
+					Expect(event.Type).To(Equal(mps.MessageReceived))
+					Expect(event.Message).To(Equal([]byte{0, 1, 2, 3, 5}))
+
+					By("waiting for the second event")
+					event = <-eventChannel
+					Expect(event.Type).To(Equal(mps.MessageReceived))
+					Expect(event.Message).To(Equal([]byte{10, 20, 30, 50, 7, 60, 70}))
+
+					By("waiting for the third event")
+					event = <-eventChannel
+					Expect(event.Type).To(Equal(mps.StreamClosed))
+				})
+			})
+
+			Context("in two reads where each read is exactly one message", func() {
+				BeforeEach(func() {
+					reader.
+						AddGoodRead([]byte{0, 1, 2, 3, 5}).
+						AddGoodRead([]byte{10, 20, 30, 50, 7, 60, 70}).
+						AddEOF()
+					go streamExtractor.StartExtracting(reader, eventChannel)
+				})
+
+				It("should emit the correct events", func() {
+					By("waiting for the first event")
+					event := <-eventChannel
+					Expect(event.Type).To(Equal(mps.MessageReceived))
+					Expect(event.Message).To(Equal([]byte{0, 1, 2, 3, 5}))
+
+					By("waiting for the second event")
+					event = <-eventChannel
+					Expect(event.Type).To(Equal(mps.MessageReceived))
+					Expect(event.Message).To(Equal([]byte{10, 20, 30, 50, 7, 60, 70}))
+
+					By("waiting for the third event")
+					event = <-eventChannel
+					Expect(event.Type).To(Equal(mps.StreamClosed))
+				})
+			})
+
+			Context("in two reads where the first read is a partial message", func() {
+				BeforeEach(func() {
+					reader.
+						AddGoodRead([]byte{0, 1}).
+						AddGoodRead([]byte{2, 3, 5, 10, 20, 30, 50, 7, 60, 70}).
+						AddEOF()
+					go streamExtractor.StartExtracting(reader, eventChannel)
+				})
+
+				It("should emit the correct events", func() {
+					By("waiting for the first event")
+					event := <-eventChannel
+					Expect(event.Type).To(Equal(mps.MessageReceived))
+					Expect(event.Message).To(Equal([]byte{0, 1, 2, 3, 5}))
+
+					By("waiting for the second event")
+					event = <-eventChannel
+					Expect(event.Type).To(Equal(mps.MessageReceived))
+					Expect(event.Message).To(Equal([]byte{10, 20, 30, 50, 7, 60, 70}))
+
+					By("waiting for the third event")
+					event = <-eventChannel
+					Expect(event.Type).To(Equal(mps.StreamClosed))
+				})
+			})
+
+			Context("in four reads", func() {
+				BeforeEach(func() {
+					reader.
+						AddGoodRead([]byte{0, 1}).
+						AddGoodRead([]byte{2, 3}).
+						AddGoodRead([]byte{5, 10, 20}).
+						AddGoodRead([]byte{30, 50, 7, 60, 70}).
+						AddEOF()
+					go streamExtractor.StartExtracting(reader, eventChannel)
+				})
+
+				It("should emit the correct events", func() {
+					By("waiting for the first event")
+					event := <-eventChannel
+					Expect(event.Type).To(Equal(mps.MessageReceived))
+					Expect(event.Message).To(Equal([]byte{0, 1, 2, 3, 5}))
+
+					By("waiting for the second event")
+					event = <-eventChannel
+					Expect(event.Type).To(Equal(mps.MessageReceived))
+					Expect(event.Message).To(Equal([]byte{10, 20, 30, 50, 7, 60, 70}))
+
+					By("waiting for the third event")
+					event = <-eventChannel
+					Expect(event.Type).To(Equal(mps.StreamClosed))
+				})
+			})
+		})
+	})
+})
